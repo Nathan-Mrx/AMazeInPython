740c8c4aa666b724e34aa058a9138c6500168f0a
meri0031 <nathan.merieux@etudiant.univ-reims.fr>
2023-03-12 15:10:55 +0100

    Changement du jeu : 4 modes de jeux et méthode Game::play_game()

--- Game.py
+++ Game.py
@@ -1,25 +1,12 @@
 from Maze import *
+from keyboard import *
+import os, time
+from math import inf
 
 class Game:
-    def __init__(self, width:int, height:int, algo:int):
-        self.width = width
-        self.height = height
-        if algo == 1:
-            self.maze = Maze.gen_btree(width, height)
-        elif algo == 2:
-            self.maze = Maze.gen_sidewinder(width, height)
-        elif algo == 3:
-            self.maze = Maze.gen_fusion(width, height)
-        elif algo == 4:
-            self.maze = Maze.gen_exploration(width, height)
-        # elif algo == 5:
-        #     self.maze = Maze.gen_wilson(width, height)
-        else:
-            raise ValueError("Algorithme invalide")
-        self.algo = algo
-        self.start = (0, 0)
-        self.end = (width - 1, height - 1)
+    def __init__(self):
         self.player_position = (0, 0)
+        self.end = (0, 0)
 
 
 
@@ -46,3 +33,122 @@ class Game:
                 (self.player_position[0] - 1, self.player_position[1]) in self.maze.neighbors[self.player_position]:
             self.player_position = (self.player_position[0] - 1, self.player_position[1])
         return self.player_position == self.end
+
+
+    def play_game(self, size, mode, algo, score = 0, game_count = 0, max_game_count = 5, random_position = False):
+        restart = False
+        stop = False
+        msg = ''
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        while game_count < max_game_count and not stop:
+
+            if game_count % 5 == 0:
+                algo = 1
+            elif game_count % 5 == 1:
+                algo = 2
+            elif game_count % 5 == 2:
+                algo = 3
+            elif game_count % 5 == 3:
+                algo = 4
+            elif game_count % 5 == 4:
+                algo = 5
+
+            if algo == 1:
+                self.maze = Maze.gen_btree(size, size)
+                msg = 'de génération par arbre binaire'
+            elif algo == 2:
+                self.maze = Maze.gen_sidewinder(size, size)
+                msg = 'sidewinder'
+            elif algo == 3:
+                self.maze = Maze.gen_fusion(size, size)
+                msg = 'de fusion de chemins'
+            elif algo == 4:
+                self.maze = Maze.gen_exploration(size, size)
+                msg = 'd\'exploration exhausitve'
+            elif algo == 5:
+                self.maze = Maze.gen_exploration(size, size)
+                msg = 'de Wilson'
+
+            if mode == 1 or mode == 2:
+                self.player_position = (0, 0)
+                self.end = (size - 1, size - 1)
+            elif mode == 3 or mode == 4:
+                cells = self.maze.get_cells()
+                self.player_position = r.choice(cells)
+                cells.remove(self.player_position)
+                self.end = r.choice(cells)
+
+            print("   _     __  __   _    _______   _                  _   _",
+                  "  /_\   |  \/  | /_\  |_  / __| (_)_ _    _ __ _  _| |_| |_  ___ _ _",
+                  " / _ \  | |\/| |/ _ \  / /| _|  | | ' \  | '_ \ || |  _| ' \/ _ \ ' \ ",
+                  "/_/ \_\ |_|  |_/_/ \_\/___|___| |_|_||_| | .__/\_, |\__|_||_\___/_||_|",
+                  "                                         |_|   |__/", sep='\n')
+
+            if max_game_count != inf:
+                print("Partie n°" + str(game_count + 1) + " sur 5")
+            else:
+                print("Partie n°" + str(game_count + 1))
+            print("Algorithme " + msg)
+
+            time.sleep(3)
+
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            print(self.maze.overlay({self.player_position: 'P', self.end: 'A'}))
+            move_count = 0
+            min_distance = self.maze.distance_geo(self.player_position, self.end)
+            won = False
+            while not won and not stop:
+                print('Appuyez sur les touches Z, Q, S, D pour vous déplacer')
+                print('Appuyez sur la touche X pour quitter')
+                print('Nombre de déplacements : ' + str(move_count))
+                print('Nombre de déplacements minimal: ' + str(min_distance))
+
+                direction = read_key()
+
+                if direction == 'x':
+                    stop = True
+                    break
+
+                old_position = self.player_position
+                won = self.move_player(direction)
+
+                if self.player_position == old_position:
+                    move_count -= 1
+
+                os.system('cls' if os.name == 'nt' else 'clear')
+
+                display_cells = {self.player_position: 'P', self.end: 'A'}
+
+                print(self.maze.overlay(display_cells))
+
+                while is_pressed('z') or is_pressed('q') or is_pressed('s') or is_pressed('d'):
+                    time.sleep(0.06)
+                move_count += 1
+
+            if won:
+                score += min_distance * 1000 / move_count
+
+                print("Vous avez gagné ! En " + str(move_count) + " déplacements !")
+                print('Votre score est de ', round(score), '/', 1000 * (game_count + 1), sep='')
+                time.sleep(1)
+                if game_count == max_game_count - 1:
+                    print("Partie terminée !")
+                    print("x pour retourner au menu")
+                    while not restart:
+                        if is_pressed('x'):
+                            restart = True
+                else:
+                    print("Prochaine partie dans 5 secondes...")
+                    time.sleep(5)
+                os.system('cls' if os.name == 'nt' else 'clear')
+
+            os.system('cls' if os.name == 'nt' else 'clear')
+            game_count += 1
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        return stop
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+from math import inf
 
 from keyboard import *
 
@@ -17,120 +18,70 @@ while not stop:
           "/_/ \_\ |_|  |_/_/ \_\/___|___| |_|_||_| | .__/\_, |\__|_||_\___/_||_|",
           "                                         |_|   |__/", sep='\n')
 
-    print("Choisissez une difficulté :")
-    print("1 - Facile")
-    print("2 - Moyen")
-    print("3 - Difficile")
-    print("4 - Expert")
-    print("5 - Impossible")
-    print("6 - Quitter")
+    print('Sélectionnez un mode de jeu :')
+    print('1 - Arcade')
+    print('2 - Endless')
+    print('3 - Random position Arcade')
+    print('4 - Random position Endless')
+    print('5 - Quitter')
 
-    difficulty = int(input("Votre choix : ")[-1])
+    mode = int(input("Votre choix : ")[-1])
 
-    size = 0
-    if difficulty == 6:
+    if mode == 5:
         stop = True
-    elif difficulty == 1:
-        size = 5
-    elif difficulty == 2:
-        size = 10
-    elif difficulty == 3:
-        size = 15
-    elif difficulty == 4:
-        size = 20
-    elif difficulty == 5:
-        size = 25
-    else:
-        raise ValueError("Difficulté invalide")
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+    if not stop:
+        print("Choisissez une difficulté :")
+        print("1 - Facile")
+        print("2 - Moyen")
+        print("3 - Difficile")
+        print("4 - Expert")
+        print("5 - Impossible")
+        print("6 - Quitter")
 
-    game_count = 0
-    score = 0
-    while game_count < 5 and not stop:
+        difficulty = int(input("Votre choix : "))
 
-        print("   _     __  __   _    _______   _                  _   _",
-              "  /_\   |  \/  | /_\  |_  / __| (_)_ _    _ __ _  _| |_| |_  ___ _ _",
-              " / _ \  | |\/| |/ _ \  / /| _|  | | ' \  | '_ \ || |  _| ' \/ _ \ ' \ ",
-              "/_/ \_\ |_|  |_/_/ \_\/___|___| |_|_||_| | .__/\_, |\__|_||_\___/_||_|",
-              "                                         |_|   |__/", sep='\n')
-
-        if game_count == 0:
-            algo = 1
-            msg = 'de génération par arbre binaire'
-        elif game_count == 1:
-            algo = 2
-            msg = 'sidewinder'
-        elif game_count == 2:
-            algo = 3
-            msg = 'de fusion de chemins'
-        elif game_count == 3:
-            algo = 4
-            msg = 'd\'exploration exhausitve'
-        elif game_count == 4:
-            algo = 4
-            # TODO: algo = 5 # A décommenter pour tester l'algorithme de Wilson
-            msg = 'de Wilson'
+        size = 0
+        if difficulty == 6:
+            stop = True
+        elif difficulty == 1:
+            size = 5
+        elif difficulty == 2:
+            size = 10
+        elif difficulty == 3:
+            size = 15
+        elif difficulty == 4:
+            size = 20
+        elif difficulty == 5:
+            size = 25
         else:
-            raise ValueError("Algorithme invalide")
+            raise ValueError("Difficulté invalide")
 
-        print("Partie n°" + str(game_count + 1) + " sur 5")
-        print("Algorithme " + msg)
+        max_game_count = 0
+        random_position = False
+        game_count = 0
+        if mode==1:
+            max_game_count = 5
+            random_position = False
 
-        time.sleep(3)
+        elif mode==2:
+            max_game_count = inf
+            random_position = False
 
-        game = Game(size, size, algo)
+        elif mode==3:
+            max_game_count = 5
+            random_position = True
 
-        os.system('cls' if os.name == 'nt' else 'clear')
+        elif mode==4:
+            max_game_count = inf
+            random_position = True
 
-        print(game.maze.overlay({game.player_position: 'P', game.end: 'A'}))
-        move_count = 0
-        min_distance = game.maze.distance_geo(game.start, game.end)
-        won = False
-        while not won and not stop:
-            print('Appuyez sur les touches Z, Q, S, D pour vous déplacer')
-            print('Appuyez sur la touche X pour quitter')
-            print('Nombre de déplacements : ' + str(move_count))
-            print('Nombre de déplacements minimal: ' + str(min_distance))
+        score = 0
 
-            direction = read_key()
+        game= Game()
+        while not stop:
 
-            if direction == 'x':
-                stop = True
+            stop = game.play_game(size, mode, 1, score, game_count, max_game_count, random_position)
 
-            old_position = game.player_position
-            won = game.move_player(direction)
-
-            if game.player_position == old_position:
-                move_count -= 1
-
-            os.system('cls' if os.name == 'nt' else 'clear')
-
-            display_cells = {game.player_position: 'P', game.end: 'A'}
-
-            print(game.maze.overlay(display_cells))
-
-            while is_pressed('z') or is_pressed('q') or is_pressed('s') or is_pressed('d'):
-                time.sleep(0.06)
-            move_count += 1
-
-        score += min_distance * 1000 / move_count
-
-        if won:
-            print("Vous avez gagné ! En " + str(move_count) + " déplacements !")
-            print('Votre score est de ', round(score), '/', 1000 * (game_count + 1), sep='')
-            time.sleep(1)
-            if game_count == 4:
-                print("Partie terminée !")
-                print("Appuyez sur x pour quitter")
-                while not restart:
-                    if is_pressed('x'):
-                        restart = True
-            else:
-                print("Prochaine partie dans 5 secondes...")
-                time.sleep(5)
-            os.system('cls' if os.name == 'nt' else 'clear')
-
-        game_count += 1
 
 os.system('cls' if os.name == 'nt' else 'clear')
